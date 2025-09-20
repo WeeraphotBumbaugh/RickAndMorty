0e4a2ac26a136868f2065d178afae0399c97075a
Will Bumbaugh <weeraphot@bumbaugh.com>
2025-09-20 12:21:35 -0700

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Weeraphot Bumbaugh on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
